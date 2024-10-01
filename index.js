85dd34a765ec82cfc6563b51634e8849db2742a3
kelvin <Kelvondambo@gmail.com>
2024-10-01 10:35:31 +0300

    index.js

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+const totalBatteries = batteryBatches.reduce(calculate);
+console.log(totalBatteries);
+
+function calculate(first, second){
+    return first + second;
+}
